12de4f34e61c0444963be4d0be067358fbfc5d1d
fghf <dghdf>
2024-05-29 15:47:01 +0300

    done

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+using namespace std;
+class A
+{ public: void f() { cout << " A"; } };
+class B: public virtual A
+{ public: void f() { cout << " B"; } };
+class C: public virtual A
+{ public: void f() { cout << " C"; } };
+class E: public virtual A, public virtual C
+{ public: void f() { cout << " E"; } };
+class D: public virtual B, public virtual C, public virtual E
+{ public: void f() { cout << " D"; } };
+class F: public virtual D, public virtual E
+{public: void f() { cout << " F"; } };
+int main()
+{
+	F object;
+	object.f();
+	object.A::f();
+	object.B::f();
+	object.C::f();
+	object.D::f();
+	object.E::f();
+}
